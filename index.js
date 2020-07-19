03d76e303cca1baab176e37d173de8875290e040
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-07-19 14:02:11 +0300

    Add class Queue

--- index.js
+++ index.js
@@ -19,3 +19,73 @@ class QueueIterator {
         }
     }
 }
+
+class Queue {
+    constructor(maxSize = 1000) {
+        this._size = 0
+        this.maxSize = maxSize
+    }
+
+    enqueue(v) {
+        this[`${this._size++}`] = v
+
+        if (this._size > this.maxSize) {
+            throw new RangeError('the queue is full')
+        }
+
+        return this._size
+    }
+
+    dequeue() {
+        if (this.isEmpty) {
+            return;
+        }
+
+        const value = this[0]
+        for (let i = 1; i < this.size; i++) {
+            this[i - 1] = this[i];
+        }
+
+        delete this[--this._size]
+        return value
+
+    }
+
+    front() {
+        if (this.isEmpty) {
+            return;
+        }
+
+        return this[0]
+    }
+
+    get isEmpty() {
+        return this.size === 0
+    }
+
+    get size() {
+        return this._size
+    }
+
+    [Symbol.iterator]() {
+        return new QueueIterator(this);
+    }
+}
+
+const queue = new Queue()
+
+for (let i = 0; i < 15; i++) {
+    queue.enqueue(i * Math.ceil(Math.random() * 10))
+}
+
+console.log(queue)
+queue.enqueue('new')
+console.log(queue)
+const arr = [...queue]
+console.log(arr)
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+console.log(queue)
+console.log(queue.isEmpty)
+console.log(queue.front())
+console.log(queue.size)
